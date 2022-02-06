64c02220de235febcb3490e19e1583c8698ab1c6
savan <savan.navalgi@gmail.com>
2022-02-06 14:27:17 +0530

    resilience4j circuitbreak features

--- currency-exchange-service/src/main/java/com/savan/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/savan/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -16,14 +18,16 @@ public class CircuitBreakerController {
 	
 	@GetMapping("/who")
 //	@Retry(name = "WHO", fallbackMethod="hardcoded")
-	@CircuitBreaker(name = "WHO", fallbackMethod="hardcoded")
+//	@CircuitBreaker(name = "WHO", fallbackMethod="hardcoded")
+	@RateLimiter(name ="WHO")
+	@Bulkhead(name="WHO")
 	public String returnHello() {
 	
 		logger.info("---------------received request-------------- / who ");
 		
-		String res = new RestTemplate().getForObject("http://localhost:8111/return", String.class);
+//		String res = new RestTemplate().getForObject("http://localhost:8111/return", String.class);
 		
-		return res;
+		return "sample-WHO";
 	}
 	
 	public String hardcoded(Exception ex) {
